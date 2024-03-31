fd4c3e183380669514bd164cf2c427e858e2f90c
nguyntinkhang <116292903+grimoiredecs@users.noreply.github.com>
2024-04-01 00:20:55 +0700

    format

--- Graph/main.hpp
+++ Graph/main.hpp
@@ -5,20 +5,18 @@
 #include <algorithm>
 #include <unordered_map>
 
-
 using namespace std;
-
-
 
 class Graph
 {
-    private:
-    void operator=(const Graph&){};
-    Graph(const Graph&){};
-    public:
+private:
+    void operator=(const Graph &){};
+    Graph(const Graph &){};
+
+public:
     Graph(){};
     virtual ~Graph(){};
-    
+
     virtual void Init(int n) = 0;
 
     virtual int n() = 0;
@@ -30,14 +28,14 @@ class Graph
     virtual void delEdge(int v1, int v2) = 0;
     virtual bool isEdge(int i, int j) = 0;
 
-    virtual int weight(int v1, int v2) =0;
+    virtual int weight(int v1, int v2) = 0;
     virtual int getMark(int v) = 0;
     virtual void setMark(int v, int val) = 0;
 
-   virtual void bfs(int start)=0;
-    virtual void dfs(int start)=0;
-    
-    void printPath(int start, int end, vector<int>& parent);
+    virtual void bfs(int start) = 0;
+    virtual void dfs(int start) = 0;
+
+    void printPath(int start, int end, vector<int> &parent);
     void dijkstra(int start);
     void prim(int start);
     void kruskal();
@@ -48,24 +46,24 @@ class Graph
     void eulerCircuit();
     void hamiltonianCycle();
     void tsp();
-
 };
 
 class Graphm : public Graph
 {
-    private:
+private:
     vector<vector<int>> matrix;
     vector<int> mark;
     int numVertex;
     int numEdge;
-    public:
-    Graphm(int n) 
+
+public:
+    Graphm(int n)
     {
         Init(n);
     }
     ~Graphm() override
     {
-        for(int i = 0; i < numVertex; i++)
+        for (int i = 0; i < numVertex; i++)
         {
             matrix[i].clear();
         }
@@ -78,13 +76,13 @@ class Graphm : public Graph
         numEdge = 0;
         mark.resize(n);
         matrix.resize(n);
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             matrix[i].resize(n);
         }
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            for(int j = 0; j < n; j++)
+            for (int j = 0; j < n; j++)
             {
                 matrix[i][j] = 0;
             }
@@ -101,20 +99,20 @@ class Graphm : public Graph
     }
     int first(int v) override
     {
-        for(int i = 0; i < numVertex; i++)
+        for (int i = 0; i < numVertex; i++)
         {
-            if(matrix[v][i] != 0)
+            if (matrix[v][i] != 0)
             {
                 return i;
             }
         }
         return numVertex;
     }
-    int next(int v, int w)  override
+    int next(int v, int w) override
     {
-        for(int i = w+1; i < numVertex; i++)
+        for (int i = w + 1; i < numVertex; i++)
         {
-            if(matrix[v][i] != 0)
+            if (matrix[v][i] != 0)
             {
                 return i;
             }
@@ -123,26 +121,26 @@ class Graphm : public Graph
     }
     void setEdge(int v1, int v2, int wght) override
     {
-        if(wght <= 0)
+        if (wght <= 0)
         {
             cout << "Illegal weight value" << endl;
             return;
         }
-        if(matrix[v1][v2] == 0)
+        if (matrix[v1][v2] == 0)
         {
             numEdge++;
         }
         matrix[v1][v2] = wght;
     }
-    void delEdge(int v1, int v2)  override
+    void delEdge(int v1, int v2) override
     {
-        if(matrix[v1][v2] != 0)
+        if (matrix[v1][v2] != 0)
         {
             numEdge--;
         }
         matrix[v1][v2] = 0;
     }
-    bool isEdge(int i, int j)   override
+    bool isEdge(int i, int j) override
     {
         return (matrix[i][j] != 0);
     }
@@ -164,14 +162,14 @@ class Graphm : public Graph
         queue<int> q;
         q.push(start);
         mark[start] = 1;
-        while(!q.empty())
+        while (!q.empty())
         {
             int v = q.front();
             q.pop();
             cout << v << " ";
-            for(int w = first(v); w < numVertex; w = next(v, w))
+            for (int w = first(v); w < numVertex; w = next(v, w))
             {
-                if(mark[w] == 0)
+                if (mark[w] == 0)
                 {
                     q.push(w);
                     mark[w] = 1;
@@ -185,13 +183,13 @@ class Graphm : public Graph
         s.push(start);
         mark[start] = 1;
         cout << start << " ";
-        while(!s.empty())
+        while (!s.empty())
         {
             int v = s.top();
             int w = first(v);
-            while(w < numVertex)
+            while (w < numVertex)
             {
-                if(mark[w] == 0)
+                if (mark[w] == 0)
                 {
                     s.push(w);
                     mark[w] = 1;
@@ -200,13 +198,10 @@ class Graphm : public Graph
                 }
                 w = next(v, w);
             }
-            if(w == numVertex)
+            if (w == numVertex)
             {
                 s.pop();
             }
         }
     }
-
-    
-
 };
